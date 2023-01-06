bfe561ccc36d714c69f8ca0d6e6a726562d1c561
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2023-01-06 08:49:47 +0700

    adding simple terminal GUI.

--- main.cpp
+++ main.cpp
@@ -5,36 +5,66 @@ int main() {
     vehicleList vehicleList{};
     adr_customer addressCustomer = nullptr;
     adr_vehicle addressVehicle = nullptr;
+    string customerName, customerID, customerStatus, vehicleBrand, vehicleType, vehicleModel, vehicleStatus;
+    int picker, vehicleReleaseYear;
 
     createListCustomer(customerList);
 
-    /*cout << "Please input how many customers: ";
-    cin >> num;
+    while (picker != 0) {
+        cout << "==== WELCOME TO BENGKEL ====" << endl;
+        cout << "1. Show All Data \n2. Show With Sample Data \n3. Add New Data \n0. Exit Program"
+             << endl;
+        cout << "Your choice: ";
+        cin >> picker;
+        cout << endl;
+        if (picker == 1) {
+//            printData(customerList);
+            if (customerList.first == nullptr) {
+                cout << "!--- The List is Empty! ---!" << endl;
+                cout << endl;
+            } else {
+                printData(customerList);
+            }
+        } else if (picker == 2) {
+            addressCustomer = CreateElementCustomerData(addressCustomer, "Fauzan", "FRM001", "Done");
+            insertLastCustomer(customerList, addressCustomer);
+            addressCustomer = CreateElementCustomerData(addressCustomer, "Alam", "ARP001", "In Progress");
+            insertLastCustomer(customerList, addressCustomer);
 
-    for (int i = 0; i < num; i++)
-    {
-        cout << "Your full name: ";
-        cin >> name;
-        cout << "ID:";
-        cin >> id;
-        cout << "Status:";
-        cin >> status;
+            addressVehicle = CreateElementVehicleData(addressVehicle, "Toyota", "SUV", "Land Cruiser", "Inspection",
+                                                      1998);
+            insertLastVehicle(customerList, vehicleList, "FRM001", addressVehicle);
+            addressVehicle = CreateElementVehicleData(addressVehicle, "Ford", "Truck", "F150", "Done", 1980);
+            insertLastVehicle(customerList, vehicleList, "ARP001", addressVehicle);
+            printData(customerList);
+        } else if (picker == 3) {
+            cout << "Customer Full Name: ";
+            cin >> customerName;
+            cout << "Customer ID (ex: IA3001): ";
+            cin >> customerID;
+            cout << "Customer Status: ";
+            cin >> customerStatus;
+            cout << "Vehicle Brand: ";
+            cin >> vehicleBrand;
+            cout << "Vehicle Type: ";
+            cin >> vehicleType;
+            cout << "vehicle Model: ";
+            cin >> vehicleModel;
+            cout << "Vehicle Status: ";
+            cin >> vehicleStatus;
+            cout << "Vehicle Release Year: ";
+            cin >> vehicleReleaseYear;
 
-        addressCustomer = CreateElementCustomerData(addressCustomer, name, id, status);
-        insertLastCustomer(customerList, addressCustomer);
+            // Perulangan jumlah kendaraan + kepemilikan
+
+            addressCustomer = CreateElementCustomerData(addressCustomer, customerName, customerID, customerStatus);
+            insertLastCustomer(customerList, addressCustomer);
+            addressVehicle = CreateElementVehicleData(addressVehicle, vehicleBrand, vehicleType, vehicleModel,
+                                                      vehicleStatus, vehicleReleaseYear);
+            insertLastVehicle(customerList, vehicleList, customerID, addressVehicle);
+            printData(customerList);
+        } else if (picker == 0) {
+            return 0;
+        }
     }
-    */
-
-    addressCustomer = CreateElementCustomerData(addressCustomer, "Fauzan", "FRM001", "Done");
-    insertLastCustomer(customerList,addressCustomer);
-    addressCustomer = CreateElementCustomerData(addressCustomer, "Alam", "ARP001", "In Progress");
-    insertLastCustomer(customerList,addressCustomer);
-
-    addressVehicle = CreateElementVehicleData(addressVehicle,"Toyota","SUV","Land Cruiser","Inspection",1998);
-    insertLastVehicle(customerList,vehicleList,"FRM001",addressVehicle);
-    addressVehicle = CreateElementVehicleData(addressVehicle,"Ford","Truck","F150","DONE",1980);
-    insertLastVehicle(customerList,vehicleList,"ARP001",addressVehicle);
-
-    printData(customerList);
-    return 0;
 }
--- menu.h
+++ menu.h
@@ -44,8 +44,6 @@ void insertLastCustomer(customerList &customerList, adr_customer addressCustomer
 adr_vehicle CreateElementVehicleData(adr_vehicle addressVehicle,string brand,string car_type,string model,string status, int year);
 void insertLastVehicle(customerList &customerList, vehicleList &vehicleList,string customer_id,adr_vehicle addressVehicle);
 adr_customer findCustomerAddress(customerList customerList,string customer_id);
-/*adr_damage CreateElementDamage(adr_damage AD,string title, string explanation, string status);
-void insertLastDamage(customerList &customerList, adr_damage AD);
-*/
+
 void printData(customerList customerList);
 #endif //MENU_H_INCLUDED
