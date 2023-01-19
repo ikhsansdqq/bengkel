826be5673f05a2c02bfc543fb64a575b4a21ae49
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2023-01-19 13:31:35 +0700

    Case 14 now added, New input ability - Now user can input with spaces (Only for Engine title, Engine Explanatio, and Customer Names) - Case 14 now added - Case 15 aka Testing have been removed because we dont need it..

--- main.cpp
+++ main.cpp
@@ -12,14 +12,17 @@ int main() {
     while (pilihan != 0) {
         switch (pilihan) {
             case 1:
+                cout << "===== INPUT CUSTOMER =====" << endl;
                 cout << "Input Customer name     : ";
-                cin >> customerName;
+                cin.ignore();
+                getline(cin,customerName,'\n');
                 cout << "Input Customer ID       : ";
                 cin >> customerID;
                 addressCustomer = CreateElementCustomerData(addressCustomer, customerName, customerID);
                 insertLastCustomer(customerList, addressCustomer);
                 break;
             case 2:
+                cout << "===== INPUT VEHICLE =====" << endl;
                 cout << "Input Vehicle Brand : ";
                 cin >> vehicleBrand;
                 cout << "Input Vehicle ID    : ";
@@ -37,14 +40,17 @@ int main() {
                 insertLastVehicle(customerList, customerID, addressVehicle);
                 break;
             case 3:
+                cout << "===== INPUT DAMAGE =====" << endl;
                 cout << "Input CustomerID   : ";
                 cin >> customerID;
                 cout << "Input VehicleID    : ";
                 cin >> vehicleID;
                 cout << "Input Damage Title : ";
-                cin >> damageTitle;
+                cin.ignore();
+                getline(cin, damageTitle,'\n');
                 cout << "Input Damage Explanation  : ";
-                cin >> damageExp;
+                cin.ignore();
+                getline(cin,damageExp,'\n');
                 cout << "Input Damage ID    : ";
                 cin >> damageID;
                 damageStat = IN_PROGRESS;
@@ -52,8 +58,7 @@ int main() {
                 insertDamage(customerList, customerID, vehicleID, addressDamage);
                 break;
             case 4:
-                cout << "========== Printing Customer =========" << endl;
-                printAllDataCustomer(customerList);
+                cout << "========== PRINT CUSTOMER =========" << endl;
                 addressCustomer = customerList.first;
                 if (addressCustomer == nullptr) {
                     cout << "There are no customers yet. Add data please." << endl;
@@ -72,12 +77,14 @@ int main() {
             case 5:
                 printAllDataCustomer(customerList);
                 break;
-            case 6:
+            case 6: 
+                cout << "========== PRINT VEHICLE =========" << endl;
                 cout << "Input CustomerID    :";
                 cin >> customerID;
                 printDataVehicle(customerList, customerID);
                 break;
             case 7:
+                cout << "========== PRINT DAMAGE =========" << endl;
                 cout << "Input CustomerID    :";
                 cin >> customerID;
                 cout << "Input VehicleID    :";
@@ -85,6 +92,7 @@ int main() {
                 printDataDamage(customerList, customerID, vehicleID);
                 break;
             case 8:
+                cout << "========== UPDATE DAMAGE =========" << endl;
                 cout << "Input CustomerID    :";
                 cin >> customerID;
                 cout << "Input VehicleID     : ";
@@ -143,115 +151,9 @@ int main() {
                 break;
             case 14:
                 cout << "==== UPDATE DATA ====" << endl;
+                updateAllCustomer(customerList);
+                cout << "==== DONE ====" << endl;
                 // UPDATING DATA
-                break;
-            case 15:
-                cout << "======= THIS IS JUST A PROCEDURE AND FUNCTION TEST =======" << endl;
-                cout << "Testing by input 2 customer data" << endl;
-                cout << "==========" << endl;
-                addressCustomer = CreateElementCustomerData(addressCustomer, "Fauzan", "FRM001");
-                insertLastCustomer(customerList, addressCustomer);
-                addressCustomer = CreateElementCustomerData(addressCustomer, "Alam", "ARP001");
-                insertLastCustomer(customerList, addressCustomer);
-                printAllDataCustomer(customerList);
-                printDataVehicle(customerList, "FRM001");
-
-                cout << endl;
-
-                cout << "Testing by input 2 vehicle data and update customer status" << endl;
-                cout << "==========" << endl;
-                addressVehicle = CreateElementVehicleData(addressVehicle, "Toyota", "TS-98", "SUV", "Land Cruiser",
-                                                          1998);
-                insertLastVehicle(customerList, "FRM001", addressVehicle);
-                addressVehicle = CreateElementVehicleData(addressVehicle, "Ford", "FT-80", "Truck", "F150", 1980);
-                insertLastVehicle(customerList, "ARP001", addressVehicle);
-                updateAllCustomer(customerList);
-                printAllDataCustomer(customerList);
-                cout << endl;
-                printDataVehicle(customerList, "FRM001");
-                printDataVehicle(customerList, "ARP001");
-                cout << endl;
-
-                cout << "Testing by input 2 vehicle data and update customer status and vehicle status" << endl;
-                cout << "==========" << endl;
-                addressDamage = CreateElementDamageData(addressDamage, "Mesin", "Salah masukin oli", IN_PROGRESS,
-                                                        "ABC");
-                insertDamage(customerList, "FRM001", "TS-98", addressDamage);
-                addressDamage = CreateElementDamageData(addressDamage, "Body", "Lecet", IN_PROGRESS, "DEF");
-                insertDamage(customerList, "ARP001", "FT-80", addressDamage);
-                updateAllCustomer(customerList);
-                printAllDataCustomer(customerList);
-                cout << endl;
-                printDataVehicle(customerList, "ARP001");
-                cout << endl;
-                printDataDamage(customerList, "ARP001", "FT-80");
-
-                cout << endl;
-
-                cout << "========= Damage Status on Vehicle Test in for ARP001 =========" << endl;
-                cout << "Before Updating" << endl;
-                printDataVehicle(customerList, "ARP001");
-                cout << "After Updating" << endl;
-                addressCustomer = findCustomerAddress(customerList, "ARP001");
-                addressVehicle = findVehicleAddress(addressCustomer, "FT-80");
-                addressDamage = findDamageAddress(addressVehicle, "DEF");
-                updateDamageStatus(addressDamage, DONE);
-                printDataDamage(customerList, "ARP001", "FT-80");
-                updateVehicleStatus(addressVehicle);
-                updateAllCustomer(customerList);
-                printDataVehicle(customerList, "ARP001");
-
-                addressDamage = findDamageAddress(addressVehicle, "DEF");
-                cout << "Delete damage to customer side (Alam)" << endl;
-                cout << "==========" << endl;
-                cout << "Before Delete" << endl;
-                printDataDamage(customerList, "ARP001", "FT-80");
-
-                cout << "After Delete" << endl;
-                addressCustomer = findCustomerAddress(customerList, "ARP001");
-                addressVehicle = findVehicleAddress(addressCustomer, "FT-80");
-                deleteDamage(addressVehicle, "DEF");
-                printDataDamage(customerList, "ARP001", "FT-80");
-
-                cout << endl;
-                cout << "Delete vehicle to customer side (Fauzan Vehicle)" << endl;
-                cout << "==========" << endl;
-
-                cout << "Before Delete" << endl;
-                printDataVehicle(customerList, "FRM001");
-
-                cout << "After Delete" << endl;
-                addressCustomer = findCustomerAddress(customerList, "FRM001");
-                deleteVehicle(addressCustomer, "TS-98");
-                printDataVehicle(customerList, "FRM001");
-
-                cout << endl;
-                cout << endl;
-
-                cout << "Delete all vechicle" << endl;
-                cout << "==========" << endl;
-
-                cout << "Before Delete" << endl;
-                printDataVehicle(customerList, "ARP001");
-
-                cout << "After Delete" << endl;
-                addressCustomer = findCustomerAddress(customerList, "ARP001");
-                deleteAllVehicle(addressCustomer);
-                printDataVehicle(customerList, "ARP001");
-                cout << endl;
-                cout << endl;
-                cout << "Delete customer to customer side (Fauzan Vechile)" << endl;
-                cout << "==========" << endl;
-
-                cout << "Before Delete" << endl;
-                printAllDataCustomer(customerList);
-
-                cout << endl;
-                cout << "After Delete" << endl;
-                deleteCustomer(customerList, "FRM001");
-                printAllDataCustomer(customerList);
-
-                cout << endl;
                 break;
         }
         cout << "Back to main menu? (Y/N) : ";
